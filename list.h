145453530cad84195cf559c2966c529e9945eede
Suvorova-Sofya <suvorova.sofya@yandex.ru>
2019-12-07 10:43:06 +0300

    изменено:      list.h

--- list.h
+++ list.h
@@ -11,7 +11,7 @@
 
 namespace containersl {
 
-    template<class T, class Allocator = std::allocator<T>>
+    template<class T>
     struct list {
     private:
         struct node;
@@ -61,24 +61,7 @@ namespace containersl {
 
     private:
 
-        using allocator_type = typename Allocator::template rebind<node>::other;
-
-        struct deleter {
-            deleter(allocator_type* allocator): allocator_(allocator) {}
-
-            void operator() (node* ptr) {
-                if(ptr != nullptr){
-                    std::allocator_traits<allocator_type>::destroy(*allocator_, ptr);
-                    allocator_->deallocate(ptr, 1);
-                }
-            }
-
-        private:
-            allocator_type* allocator_;
-
-        };
-
-        using unique_ptr = std::unique_ptr<node, deleter>;
+        using unique_ptr = std::unique_ptr<node>;
 
         node *end_node = nullptr;
 
@@ -86,31 +69,29 @@ namespace containersl {
 
         struct node {
             T value;
-            unique_ptr next{nullptr, deleter{nullptr}};
+            unique_ptr next{nullptr};
             node *parent = nullptr;
             forward_iterator nextf();
         };
 
-        allocator_type allocator_{};
-
-        unique_ptr root{nullptr, deleter{nullptr}};
+        unique_ptr root{nullptr};
 
     };
 
 
 //
 
-    template<class T,class Allocator>
-    typename list<T,Allocator>::node *list<T,Allocator>::end_help(containersl::list<T,Allocator>::node *ptr) {
+    template<class T>
+    typename list<T>::node *list<T>::end_help(containersl::list<T>::node *ptr) {
         if ((ptr == nullptr) || (ptr->next == nullptr)) {
             return ptr;
         }
-        return list<T,Allocator>::end_help(ptr->next.get());
+        return list<T>::end_help(ptr->next.get());
     }
 
 
-    template<class T,class Allocator>
-    typename list<T,Allocator>::forward_iterator list<T,Allocator>::begin() {
+    template<class T>
+    typename list<T>::forward_iterator list<T>::begin() {
         if (root == nullptr) {
             return nullptr;
         }
@@ -118,14 +99,14 @@ namespace containersl {
         return it;
     }
 
-    template<class T,class Allocator>
-    typename list<T,Allocator>::forward_iterator list<T,Allocator>::end() {
+    template<class T>
+    typename list<T>::forward_iterator list<T>::end() {
         return nullptr;
     }
 
-    template<class T,class Allocator>
-    void list<T,Allocator>::insert(const list<T,Allocator>::forward_iterator &it, const T &value) {
-        std::unique_ptr<node,list<T, Allocator>::deleter> new_node{new node{value},deleter{&allocator_}};
+    template<class T>
+    void list<T>::insert(const list<T>::forward_iterator &it, const T &value) {
+        std::unique_ptr<node> new_node{new node{value}};
         if (it != nullptr) {
             node *ptr = it.ptr_->parent;
             new_node->parent = it.ptr_->parent;
@@ -142,7 +123,7 @@ namespace containersl {
             if(end_node==nullptr) {
                 new_node->parent= nullptr;
                 new_node->next= nullptr;
-                list<T,Allocator>::root = std::move(new_node);
+                list<T>::root = std::move(new_node);
             }else{
                 new_node->parent=end_node;
                 new_node->next= nullptr;
@@ -153,8 +134,8 @@ namespace containersl {
         ++size;
     }
 
-    template<class T,class Allocator>
-    void list<T,Allocator>::erase(const list<T,Allocator>::forward_iterator &it) {
+    template<class T>
+    void list<T>::erase(const list<T>::forward_iterator &it) {
         if (it.ptr_ == nullptr) {
             throw std::logic_error("erasing invalid iterator");
         }
@@ -165,28 +146,27 @@ namespace containersl {
             return it.ptr_->parent->next;
         }();
         pointer_from_parent = std::move(it.ptr_->next);
-
         end_node = end_help(root.get());
         --size;
     }
 
 //
-    template<class T,class Allocator>
-    typename list<T,Allocator>::forward_iterator list<T,Allocator>::node::nextf() {
+    template<class T>
+    typename list<T>::forward_iterator list<T>::node::nextf() {
         forward_iterator result(this->next.get());
         return result;
     }
 
-    template<class T,class Allocator>
-    list<T,Allocator>::forward_iterator::forward_iterator(node *ptr): ptr_{ptr} {}
+    template<class T>
+    list<T>::forward_iterator::forward_iterator(node *ptr): ptr_{ptr} {}
 
-    template<class T,class Allocator>
-    T &list<T,Allocator>::forward_iterator::operator*() {
+    template<class T>
+    T &list<T>::forward_iterator::operator*() {
         return ptr_->value;
     }
 
-    template<class T,class Allocator>
-    typename list<T,Allocator>::forward_iterator &list<T,Allocator>::forward_iterator::operator++() {
+    template<class T>
+    typename list<T>::forward_iterator &list<T>::forward_iterator::operator++() {
         if (*this != nullptr) {
             *this = ptr_->nextf();
             return *this;
@@ -195,21 +175,21 @@ namespace containersl {
         }
     }
 
-    template<class T,class Allocator>
-    typename list<T,Allocator>::forward_iterator list<T,Allocator>::forward_iterator::operator+(int r) {
+    template<class T>
+    typename list<T>::forward_iterator list<T>::forward_iterator::operator+(int r) {
         for (int i = 0; i < r; ++i) {
             ++*this;
         }
         return *this;
     }
 
-    template<class T,class Allocator>
-    bool list<T,Allocator>::forward_iterator::operator==(const forward_iterator &o) const {
+    template<class T>
+    bool list<T>::forward_iterator::operator==(const forward_iterator &o) const {
         return ptr_ == o.ptr_;
     }
 
-    template<class T,class Allocator>
-    bool list<T,Allocator>::forward_iterator::operator!=(const forward_iterator &o) const {
+    template<class T>
+    bool list<T>::forward_iterator::operator!=(const forward_iterator &o) const {
         return ptr_ != o.ptr_;
     }
 
